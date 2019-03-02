ec2f17496ed4dd63ea1d3fd8e8e0d546da1c1440
Sanjana <sh9as@virginia.edu>
2019-03-02 17:37:24 -0500

    models

--- bank/banks/models.py
+++ bank/banks/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Customer(models.Model):
+	first_name = models.CharField(max_length = 200)
+	last_name = models.CharField(max_length = 200)
+	zip_code = models.CharField(max_length = 10)
+	email = models.EmailField()
+
+class Account(models.Model):
+	balance= models.CharField(max_length = 200)
+	account_number= models.CharField(max_length= 200)
+	customer= models.ForeignKey(Customer, on_delete= models.CASCADE)
+
+
+
+
+
